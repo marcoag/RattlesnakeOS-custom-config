02e1bc30bc447f3ff1bf2b2cda4ddc02edd38faa
Marco A. Gutierrez <marcogg@marcogg.com>
2021-06-13 13:49:50 +0800

    fix typo on seedvault community patch
    Signed-off-by: Marco A. Gutierrez <marcogg@marcogg.com>

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -13,7 +13,7 @@ rm -rf "${community_patches_dir}"
 git clone https://github.com/rattlesnakeos/community_patches "${community_patches_dir}"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00004-use-cloudflare-dns.patch"
-patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/0007-set-seedvault-as-dftl-bkp-provider.patch"
+patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00007-set-seedvault-as-dftl-bkp-provider.patch"
 
 # apply custom hosts file
 custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
